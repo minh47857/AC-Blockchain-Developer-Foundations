e3025f6cb694cf47b1f2a45a774d1b5e7fd00475
minh47857 <minh47857@gmail.com>
2026-03-09 18:25:47 +0700

    finish bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import crypto from "crypto";
 
 export type Block = {
   index: number;
@@ -9,5 +10,7 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const value = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const hash: string =  crypto.createHash('sha256').update(value).digest('hex');
+  return block.current_hash === hash;
 }
